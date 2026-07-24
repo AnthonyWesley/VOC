834b1c4d5f44c94b14e997400a0efade337bd13b
Anthony Wesley <awl-7@live.com>
2026-07-24 17:10:57 -0300

    chore: remove SQLite migrations from active path after archival

--- VOC-api/prisma/migrations/20260720_add_password_lifecycle/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "temporaryPasswordExpiresAt" DATETIME;
-ALTER TABLE "User" ADD COLUMN "passwordChangedAt" DATETIME;
--- VOC-api/prisma/migrations/20260720_backfill_reversal_semantics/migration.sql
+++
@@ -1,5 +0,0 @@
--- Migration 2: Backfill sem?ntico
--- Data migration run manually. No schema changes.
--- Schema at this point: direction is nullable, reversal fields added.
-SELECT 1;
-
